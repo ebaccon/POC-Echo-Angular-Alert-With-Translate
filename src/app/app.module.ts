b3d0de137e3c82509cb6c081b43d80e01aa27072
Erwan Baccon <erwan.baccon@gmail.com>
2017-05-11 19:54:24 +0200

    [EBA] - Add ngx-translate dependencies - Add ngx-configuration - Add translate in "app.component.html" - Add switch beetween langage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
+
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: Http) {
+    return new TranslateHttpLoader(http, "assets/i18n/", ".json");
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +20,14 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [Http]
+        }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
